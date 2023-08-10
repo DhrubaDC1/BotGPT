c49a24c72a25ce6e3082930ab908312c59ffb43f
dhrubaDC1 <dhruba.chakraborty.2000@gmail.com>
2023-08-11 00:37:12 +0600

    feat: :sparkles: Added google bard api, implemented response via telegram api

--- app.py
+++ app.py
@@ -2,24 +2,35 @@
 import openai
 import telebot
 import re
+import bardapi
+import os
 
 global TOKEN
 TOKEN = '6591453315:AAFZ2nlZgRP9mcUPRES4OEzQyJzFsCa_NA0'
 bot = telebot.TeleBot(TOKEN)
 
-openai.api_key = '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'
 
-messages = [{'role': 'system', 'content': 'You are a kind helpful assistant.'}]
-
-# @bot.message_handler(commands=['Greet'])
-# def greet(message): {
-#     bot.reply_to(message, "Hello! I'm your way to ChatGPT")
-# }
+# set your __Secure-1PSID value to key
+token_bard = 'Zwi46l42B9zXFKWqQnCK194pBgfZLWXiqS8QyVsdinNGZC68nTxLE9lYCQ23u6eo7MN56Q.'
 
 
-# @bot.message_handler(func=lambda message: True)
-# def echo_message(message):
-#     bot.reply_to(message, message.text)
+@bot.message_handler(commands=['Greet'])
+def greet(message): {
+    bot.reply_to(message, "Hello! I'm your way to Bard")
+}
 
 
-# bot.polling()
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, 'On it. Please wait...')
+    # set your input text
+    input_text = message.text
+    print(input_text)
+
+    # Send an API request and get a response.
+    response = bardapi.core.Bard(token_bard).get_answer(input_text)['content']
+    print(response)
+    bot.reply_to(message, response)
+
+
+bot.polling()
